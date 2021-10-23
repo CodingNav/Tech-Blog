83cfb077a2130719c95cad9c79615e69e3a9a68e
Navleen <navleenkaurr@gmail.com>
2021-10-22 23:46:24 -0400

    Fixed password error

--- public/js/login.js
+++ public/js/login.js
@@ -25,10 +25,11 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
+    console.log(document.querySelector('#password-signup').value);
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (username && email && password) {
+    if (username && password) {
         const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({ username, password }),
