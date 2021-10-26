49161bc35ddb9e70ea7dfa4bd44e92eb24bec240
Navleen <navleenkaurr@gmail.com>
2021-10-26 17:02:38 -0400

    Removed test key value

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -17,8 +17,7 @@ router.get('/', async (req, res) => {
     // Pass serialized data and session flag into template
     res.render('homepage', { 
       posts, 
-      logged_in: req.session.logged_in,
-      test: 'test'
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
