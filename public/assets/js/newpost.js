78c03963c75c3a2fb036758cc271c540762aa41b
Navleen <navleenkaurr@gmail.com>
2021-10-26 18:33:55 -0400

    Added newPostHandler function

---
+++ public/assets/js/newpost.js
@@ -0,0 +1,24 @@
+const newPostHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title-input').value.trim();
+    const message = document.querySelector('#message-input').value.trim();
+
+    if (title && message) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ title, message }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    };
+}
+
+document
+    .querySelector('.newPost-form')
+    .addEventListener('submit', newPostHandler);
