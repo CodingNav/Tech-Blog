89e60d49f14ba875f66383e6ee191a725a25d324
Navleen <navleenkaurr@gmail.com>
2021-10-26 16:44:57 -0400

    Added signout btn with functionalities

---
+++ public/assets/js/signout.js
@@ -0,0 +1,16 @@
+const signoutHandler = async () => {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/login');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document
+    .querySelector('#signout-btn')
+    .addEventListener('click', signoutHandler);
