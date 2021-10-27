bf8394ba724e70f145f7935bb46d1fdf804879f9
Navleen <navleenkaurr@gmail.com>
2021-10-26 20:33:34 -0400

    Added Comment model and set up associations

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -36,7 +36,7 @@ router.get('/post/:id', async (req, res) => {
     });
 
     const post = postData.get({ plain: true });
-
+    console.log(post);
     res.render('post', {
       ...post,
       logged_in: req.session.logged_in
--- models/index.js
+++ models/index.js
@@ -2,8 +2,19 @@ const User = require('./User');
 const Post = require('./Post');
 
 User.hasMany(Post);
+
 Post.belongsTo(User, {
     foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment);
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
 });
 
 module.exports = { User, Post };
