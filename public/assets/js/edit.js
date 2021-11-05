00f00c262f052b0d7ea2d7a9c6f97912d0a52829
Navleen <navleenkaurr@gmail.com>
2021-11-05 12:49:07 -0400

    Set up editPostHandler

---
+++ public/assets/js/edit.js
@@ -0,0 +1,25 @@
+const editPostHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title-input').value.trim();
+    const message = document.querySelector('#message-input').value.trim();
+    const id = document.querySelector('editPost-form').dataset.id;ß
+
+    if (title && message) {
+        const response = await fetch('/api/posts/' + id, {
+            method: 'PUT',
+            body: JSON.stringify({ title, message }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    };
+}
+
+document
+    .querySelector('.editPost-form')
+    .addEventListener('submit', editPostHandler);
